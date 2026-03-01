d58ec9507d6bb6437e5bdcd2b4ebd2c049ee3c1d
ItIsEvan <113439863+ItIsEvan@users.noreply.github.com>
2026-03-01 15:45:52 -0500

    FOSS Release v1.2.6

--- objects/pause_menu/other_elements/gag_panel.gd
+++ objects/pause_menu/other_elements/gag_panel.gd
@@ -4,15 +4,11 @@ extends Control
 @onready var gag_name : Control = %GagName
 @onready var gag_stats : Control  = %GagStats
 
+
 func _ready() -> void:
-	for track : TrackElement in %Tracks.get_children():
-		if not track.track:
-			continue
-		for button : GagButton in track.gag_buttons:
-			button.mouse_entered.connect(display_gag.bind(track.track.gags[track.gag_buttons.find(button)]))
-			button.mouse_exited.connect(display_gag)
 	display_gag()
-	sync_player_info() 
+	sync_player_info()
+	refresh()
 
 func display_gag(gag: ToonAttack = null) -> void:
 	if not gag:
@@ -26,9 +22,9 @@ func display_gag(gag: ToonAttack = null) -> void:
 		gag_stats.set_text(gag.get_stats())
 		show_player_info(false)
 
-func show_player_info(_show: bool) -> void:
-	%PlayerInfo.visible = _show
-	%GagInfo.visible = not _show
+func show_player_info(enabled: bool) -> void:
+	%PlayerInfo.visible = enabled
+	%GagInfo.visible = not enabled
 
 func sync_player_info() -> void:
 	%PlayerInfoLabel.set_text(
@@ -36,5 +32,36 @@ func sync_player_info() -> void:
 	)
 
 func refresh() -> void:
-	for track : TrackElement in %Tracks.get_children():
+	for track: TrackElement in %Tracks.get_children():
 		track.refresh()
+		if not track.track:
+			continue
+		for button: GagButton in track.gag_buttons:
+			if button.mouse_entered.is_connected(display_gag):
+				button.mouse_entered.disconnect(display_gag)
+				button.mouse_exited.disconnect(display_gag)
+				button.pressed.disconnect(gag_pressed)
+			button.mouse_entered.connect(display_gag.bind(track.track.gags[track.gag_buttons.find(button)]))
+			button.mouse_exited.connect(display_gag)
+			button.pressed.connect(gag_pressed.bind(track.track, track.gag_buttons.find(button)))
+	
+
+func gag_pressed(track: Track, idx: int) -> void:
+	if not is_instance_valid(Util.get_player()): return
+	if not Util.get_player().alt_gag_hotswap: return
+	
+	var gag_variants := track.get_gag_variants(idx)
+	if gag_variants.size() == 1: return
+	
+	var gag_index := gag_variants.find(track.gags[idx])
+	var new_index := gag_index + 1
+	if new_index >= gag_variants.size(): new_index = 0
+	track.swap_gag(gag_variants[new_index])
+	refresh()
+
+func get_buttons() -> Dictionary[GagButton, ToonAttack]:
+	var buttons: Dictionary[GagButton, ToonAttack] = {}
+	for track: TrackElement in %Tracks.get_children():
+		for button: GagButton in track.gag_buttons:
+			buttons[button] = track.gags[track.gag_buttons.find(button)]
+	return buttons
